6058fcbf878f1b547864db2886161b839ff37b18
Daniel Ferreira <daniel@ferreira.fm>
2018-11-06 10:38:35 +0100

    adding MFA support

--- Get-MailboxAudit.ps1
+++ Get-MailboxAudit.ps1
@@ -1,72 +1,70 @@
 function Get-MailboxAudit {
-
     <#
-    .SYNOPSIS
-        Returns the delegated permissions of a given mailbox in an Office 365 account. Required Admin account at tenant level.
 
-    .PARAMETER Credential
-        The secure credential object.
-        
-        PSCredential. Mandatory.
     #>   
-
     [CmdletBinding()]
     [OutputType([psobject])]
-    param(    
-        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$false)]
-        [System.Management.Automation.PSCredential] $Credential,
-
-        [Parameter(Position=1, Mandatory=$false, ValueFromPipeline=$true)]
+    param(
+        [Parameter(Position=0, Mandatory=$false, ValueFromPipeline=$true)]
+        [Alias("mail")] 
         [string] $Identity,
 
-        [Parameter(Position=2, Mandatory=$false, ValueFromPipeline=$false)]
-        [Switch] $Proxy
+        [Parameter(Position=1, Mandatory=$false, ValueFromPipeline=$false)]
+        [System.Management.Automation.PSCredential] $Credential,
 
-        #TODO: receive a pipeline array to proccess only given accounts. 
+        [Parameter(Position=2, Mandatory=$false, ValueFromPipeline=$false)]
+        [Switch] $Proxy,
+
+        [Parameter(Position=3, Mandatory=$false, ValueFromPipeline=$false)]
+        [Switch] $MFA
     )
 
     begin {
 
-        ## Remove all existing Powershell sessions 
-        Get-PSSession | Remove-PSSession | Out-Null 
-
         ## Connect to O365:
         Write-Verbose "[Get-MailboxAudit] Connecting to the tenant using provided credentials..."
 
-        if ($Proxy) { 
-            Write-Verbose "[Get-MailboxAudit] You supplied a proxy switch. Make sure the proxy setting is configured in Internet Explorer configuration and that you can browser the web."
-
-            $proxyOptions = New-PSSessionOption -ProxyAccessType IEConfig
-            $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection -SessionOption $proxyOptions -ErrorAction Stop
+        if ($MFA) {
+            $tenantadmincommand = Get-Command -Name Get-MailboxPermission -EA 0
+            if (-not($tenantadmincommand)) { Write-Error 'ERROR: you specified to be MFA. You have to run the coomand "$proxyOptions = New-PSSessionOption -ProxyAccessType IEConfig; Connect-EXOPSSession -UserPrincipalName <upn> -PSSessionOption $proxyOptions", under a "Microsoft Exchange Online Remote PowerShell Module" console.' -ErrorAction Stop }
+            else { Write-Verbose "[Get-MailboxAudit] Connected." }
         }
-        else { $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection -ErrorAction Stop }
-        
-        if ($null -ne $session) { 
-            Import-PSSession $Session -DisableNameChecking -AllowClobber | Out-Null
-            Write-Verbose "[Get-MailboxAudit] Connected."
+        else {
+            if ($Proxy) { 
+                Write-Verbose "[Get-MailboxAudit] You supplied a proxy switch. Make sure the proxy setting is configured in Internet Explorer configuration and that you can browser the web."
+    
+                $proxyOptions = New-PSSessionOption -ProxyAccessType IEConfig
+                $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection -SessionOption $proxyOptions -ErrorAction Stop
+            }
+            else { $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection -ErrorAction Stop }
+            
+            if ($null -ne $session) { 
+                Import-PSSession $Session -DisableNameChecking -AllowClobber | Out-Null
+                Write-Verbose "[Get-MailboxAudit] Connected."
+    
+                $tenantadmincommand = Get-Command -Name Get-MailboxPermission -EA 0
+                if (-not($tenantadmincommand)) { Write-Error "ERROR: couldn't find all cmdlets from the PSSession connection. Normally due to not enough rights at the tenant. Try a different account." -ErrorAction Stop }
+            }
+            else { Write-Error "[Get-MailboxAudit] Can't connect. Check if you're behind a proxy and if so, configure the proxy settings in Internet Explorer first." }
         }
-        else { Write-Error "[Get-MailboxAudit] Can't connect. Check if you're behind a proxy and if so, configure the proxy settings in Internet Explorer first." }
         
     }
 
     process {
         # Retreiving emails of the tenant:
-        Write-Verbose "[Get-MailboxAudit] Retreiving target emails from the tenant..."
+        if ($Identity) { Write-Verbose "[Get-MailboxAudit] Listing permissions for user: $Identity" }
+        else { Write-Verbose "[Get-MailboxAudit] Retreiving target emails from the tenant..." }
         
-        $users = if ($Identity) { Get-Mailbox -Identity $Identity } else { Get-Mailbox -ResultSize unlimited | Where-Object {$_.PrimarySmtpAddress -notmatch '{'} }
-
-        $users_count = ($users | Measure-Object).Count
-        Write-Verbose "[Get-MailboxAudit] Users received. Quantity: $users_count."
+        $users = if ($Identity) { Get-Mailbox -Identity $Identity -ErrorAction silentlycontinue } else { Get-Mailbox -ResultSize unlimited | Where-Object {$_.PrimarySmtpAddress -notmatch '{'} }
 
         # Listing out Mailbox level permissions
-        Write-Verbose "[Get-MailboxAudit] Listing out Mailbox level permissions..."
         $users | ForEach-Object {
             # Grab the user primary email:
             $user_primarysmtpaddress = $_.PrimarySmtpAddress
             $user_emaildomain = [string](([regex]::Match($user_primarysmtpaddress, "(?<domain>@(\w+\.)+(\w+)?$)")).groups["domain"].value)
 
             #
-            # Grab the mailbox permissions:
+            # Grab the Mailbox permissions:
             #
             $mailboxpermissions = Get-MailboxPermission -ResultSize unlimited -Identity $user_primarysmtpaddress | Where-Object {$_.isinherited -ne $true -and $_.user -notmatch 'SELF$' -and $_.isvalid -eq $true -and $_.deny -eq $false} 
             # Output:
@@ -111,5 +109,8 @@ function Get-MailboxAudit {
             
         }
     }
-    end { }
+    end { 
+        Get-PSSession | Remove-PSSession | Out-Null 
+        Write-Verbose "[Get-MailboxAudit] Completed."
+    }
 }
